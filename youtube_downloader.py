d4ac0a7167422bdaeac4724a55ca8180fa7fce1e
Hello There <hello.there.reader.how.are.you@gmail.com>
2024-07-08 20:00:38 +0300

    off_camera_minning

--- youtube_downloader.py
+++
@@ -1,9 +0,0 @@
-from youtube_search import YoutubeSearch
-from pytube import YouTube 
-
-SAVE_PATH = "./"
-
-def yt_dl(search_term):
-    YouTube(f"https://youtube.com{YoutubeSearch(search_term, max_results=1).to_dict()[0]["url_suffix"]}").streams.filter(file_extension='mp4').all()[-1].download(output_path=SAVE_PATH)
-
-
