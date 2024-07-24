3467d07049d6673320bd7b9a93454ee6eed93e6c
Hello There <hello.there.reader.how.are.you@gmail.com>
2024-07-24 19:48:28 +0300

    YAY

--- clock.py
+++ clock.py
@@ -6,6 +6,9 @@ import threading
 ALARM_SOUND = "alarm_bells.mp3"
 
 class clock:
+    def __init__(self, volume=200):
+        self.volume = volume
+
     #clock
     def now():
         return datetime.datetime.now()
@@ -25,30 +28,52 @@ class clock:
             day = datetime.datetime.now().strftime("%d"[1])
         return f"{datetime.datetime.now().strftime("%A")} the {num2words(day, "ordinal_num")} of {datetime.datetime.now().strftime("%B")} the year of our lord {num2words(datetime.datetime.now().strftime("%C"))} {num2words(datetime.datetime.now().strftime("%y"))}"
 
+    def play_alarm_sound(self):
+        vlc_instance = vlc.Instance()
+        player = vlc_instance.media_player_new()
+        media = vlc_instance.media_new(ALARM_SOUND)
+        player.set_media(media)
+        player.audio_set_volume(self.volume)
+        player.play()
+        time.sleep(0.2)
+        duration = player.get_length()/1000
+        time.sleep(duration)
+
+
     #timer
-    def timer(wait):
-        #accepts seconds
+    def timer(self, wait, label="timer"):
+        #accepts seconds (int)
         #datetime.datetime.now() + datetime.timedelta(seconds=wait)
-        def play_alarm_sound(wait):
+        def funk_timer(wait):
             time.sleep(wait)
-            vlc_instance = vlc.Instance()
-            player = vlc_instance.media_player_new()
-            media = vlc_instance.media_new(ALARM_SOUND)
-            player.set_media(media)
-            player.audio_set_volume(500)
-            player.play()
-            time.sleep(0.2)
-            duration = player.get_length()/1000
-            time.sleep(duration)
-        threading.Thread(target=play_alarm_sound(wait), daemon=True).start()
+            self.play_alarm_sound()
+        threading.Thread(target=funk_timer, args=(wait,), daemon=True).start()
+
+    def alarm(self, trigger_time, label="alarm"):
+        update_rate = 1
+        #label does not work
+        #expects format(millatry):   hour:minute        
+        def funk_alarm(trigger_time):
+            while True:
+                if trigger_time.split(":")[0] == datetime.datetime.now().strftime("%H"):
+                    if trigger_time.split(":")[1] == datetime.datetime.now().strftime("%M"):
+                        self.play_alarm_sound()
+                        return
+                time.sleep(update_rate)
+        threading.Thread(target=funk_alarm, args=(trigger_time,), daemon=True).start()
 
 
 
 
-
+"""
 x = clock
-
-
 print(x.timer(5))
 
+x=clock().alarm(datetime.datetime.now().strftime("%H:%M"))
+
+clock().alarm("18:20")
+clock().timer(5)
+print("MEEE")
+time.sleep(4000000)
+"""
 
--- youtube.py
+++ youtube.py
@@ -1,30 +1,96 @@
 from youtube_search import YoutubeSearch
-from pytube import YouTube 
 import vlc
 import time
 import os
+import yt_dlp
+import threading
 
-SAVE_PATH = "./"
+#Where does the video file go? It dissapears...
 
-def yt_dl(search_term):
-    downloaded_file = YouTube(f"https://youtube.com{YoutubeSearch(search_term, max_results=1).to_dict()[0]["url_suffix"]}").streams.filter(file_extension='mp4').all()[-1].download(output_path=SAVE_PATH)
-    return downloaded_file[downloaded_file.find('./') + 2:]
+class yt:
+    def __init__(self, search_term=None, is_paused=False):
+        self.file_title = "yt_vid"
+        self.format = "m4a"
+        self.savepath = "./"
+        self.Full_Name = f"{self.savepath}{self.file_title}.{self.format}"
 
+        self.is_paused = is_paused
+        self.search_term = search_term
 
+        ydl_opts = {
+            'outtmpl': self.file_title,
+            'format': f'{self.format}/bestaudio/best',
+            # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+            'postprocessors': [{  # Extract audio using ffmpeg
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': f'{self.format}',
+            }]
+        }
+        self.ydl = yt_dlp.YoutubeDL(ydl_opts)
 
-def yt_play(search_term):
-    downloaded_file = YouTube(f"https://youtube.com{YoutubeSearch(search_term, max_results=1).to_dict()[0]["url_suffix"]}").streams.filter(file_extension='mp4').all()[-1].download(output_path=SAVE_PATH)
-    source = downloaded_file[downloaded_file.find('./') + 2:]
-    vlc_instance = vlc.Instance()
-    player = vlc_instance.media_player_new()
-    media = vlc_instance.media_new(source)
-    player.set_media(media)
-    player.play()
-    time.sleep(0.2)
-    duration = player.get_length()/1000
-    time.sleep(duration)
-    os.remove(source)
+        self.vlc_instance = vlc.Instance()
+        self.player = self.vlc_instance.media_player_new()
 
+        disc = threading.Thread(target=self.core, daemon=True)
+        disc.start()
+
+    def set_search_term(self, search_term):
+        self.search_term = search_term
+    def wait_for_search_term(self):
+        while self.search_term == None:
+            time.sleep(0.1)
     
-yt_play("video killed the radio star")
+    def play(self):
+        if self.search_term == None: return
+        self.is_paused = False
+        self.player.play()
+    def pause(self):
+        if self.search_term == None: return
+        self.is_paused = True
+        self.player.pause()
+
+
+    def core(self):
+        self.wait_for_search_term()
+
+        try: os.remove(self.Full_Name)
+        except: pass
+        print(self.search_term)
+        self.ydl.download(f"https://youtube.com{YoutubeSearch(self.search_term, max_results=1).to_dict()[0]["url_suffix"]}")
+
+
+        self.media = self.vlc_instance.media_new(self.Full_Name)
+        self.player.set_media(self.media)
+
+        self.player.play()
+        length = self.player.get_length()/1000
+
+        time.sleep(0.2)
+
+        tick = 0
+
+        while tick <= length:
+            if self.is_paused == True:
+                time.sleep(0.1)
+
+            else:
+                time.sleep(0.1)
+                tick +=1
+        os.remove(self.Full_Name)
+
+
+
+"""
+jukebox = yt("radio gaga")
+time.sleep(15)
+jukebox.pause()
+input("continue?:")
+jukebox.play()
+
+time.sleep(100)
 print("done")
+"""
+
+
+
+
