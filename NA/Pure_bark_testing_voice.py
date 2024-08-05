999099623849a48705339660cee4d3b5759faa08
Hello There <hello.there.reader.how.are.you@gmail.com>
2024-08-05 17:59:26 +0300

    we do what we must because we can

---
+++ NA/Pure_bark_testing_voice.py
@@ -0,0 +1,69 @@
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+from IPython.display import Audio
+
+import vlc
+import time
+import os
+import threading
+
+#https://suno-ai.notion.site/8b8e8749ed514b0cbf3f699013548683?v=bc67cff786b04b50b3ceb756fd05f68c&p=298639784299443090b6870cbcafde69&pm=s
+
+#on macos must run: export SUNO_ENABLE_MPS=True
+#check with echo $SUNO_ENABLE_MPS
+#or:
+#os.environ["SUNO_ENABLE_MPS"] = "True"
+os.environ["SUNO_ENABLE_MPS"] = "True"
+os.environ["SUNO_USE_SMALL_MODELS"] = "True"
+
+
+file_name = "voice_lines.wav"
+Full_Name = f"./{file_name}"
+
+voice = "v2/en_speaker_6"
+
+
+
+#preload_models()
+
+
+def speak(lines):
+     audio_array = generate_audio(lines, history_prompt=voice)
+     write_wav(file_name, SAMPLE_RATE, audio_array)
+     disc = threading.Thread(target=clv_play, args=(Full_Name,), daemon=True)
+     disc.start()
+
+def clv_play(file_path):
+    vlc_instance = vlc.Instance()
+    player = vlc_instance.media_player_new()
+    media = vlc_instance.media_new(file_path)
+    player.set_media(media)
+    player.play()
+    time.sleep(0.2)
+    duration = player.get_length()/1000
+    time.sleep(duration)
+    #os.remove(file_path)
+
+
+from llama_cpp import Llama
+import time
+from testing_prompt import *
+#alt is best version with corrections
+
+#--override-kv tokenizer.ggml.pre=str:tekken
+llm = Llama("./models/dolphin-2.9.3-mistral-nemo-12b.Q5_K_M.gguf", n_gpu_layers=-1)
+
+print("\n\n\n")
+def ask(question):
+      x = llm(
+            BROMPT_GRAIN.format(question),
+            #temperature=0.3
+            max_tokens=400,
+            seed=420,
+            stop=["<|end|>", "<|im_end|>"],
+      )["choices"][0]["text"]
+      return x
+
+speak(ask("Please turn my AC on"))
+
+time.sleep(1000)
---
+++ modules/clock.py
@@ -0,0 +1,79 @@
+import datetime
+from num2words import num2words
+import time
+import vlc
+import threading
+ALARM_SOUND = "alarm_bells.mp3"
+
+class clock:
+    def __init__(self, volume=200):
+        self.volume = volume
+
+    #clock
+    def now(self):
+        return datetime.datetime.now()
+    def time(self):
+        return datetime.datetime.now().strftime("%I:%M:%S %p")
+    def time_pretty(self):
+        return f"{num2words(datetime.datetime.now().strftime("%I"))} {num2words(datetime.datetime.now().strftime("%M"))} {datetime.datetime.now().strftime("%p")} "
+    def time_military(self):
+        return datetime.datetime.now().strftime("%H:%M%S")
+    def day(self):
+        return datetime.datetime.now().today()
+    def date(self):
+        return datetime.datetime.now().strftime("%m/%d/%Y")
+    def date_words(self):
+        day = datetime.datetime.now().strftime("%d")
+        if datetime.datetime.now().strftime("%d"[0]) == "0":
+            day = datetime.datetime.now().strftime("%d"[1])
+        return f"{datetime.datetime.now().strftime("%A")} the {num2words(day, "ordinal_num")} of {datetime.datetime.now().strftime("%B")} the year of our lord {num2words(datetime.datetime.now().strftime("%C"))} {num2words(datetime.datetime.now().strftime("%y"))}"
+
+    def play_alarm_sound(self):
+        vlc_instance = vlc.Instance()
+        player = vlc_instance.media_player_new()
+        media = vlc_instance.media_new(ALARM_SOUND)
+        player.set_media(media)
+        player.audio_set_volume(self.volume)
+        player.play()
+        time.sleep(0.2)
+        duration = player.get_length()/1000
+        time.sleep(duration)
+
+
+    #timer
+    def timer(self, wait, label="timer"):
+        #accepts seconds (int)
+        #datetime.datetime.now() + datetime.timedelta(seconds=wait)
+        def funk_timer(wait):
+            time.sleep(wait)
+            self.play_alarm_sound()
+        threading.Thread(target=funk_timer, args=(wait,), daemon=True).start()
+
+    def alarm(self, trigger_time, label="alarm"):
+        update_rate = 1
+        #label does not work
+        #expects format(millatry):   hour:minute        
+        def funk_alarm(trigger_time):
+            while True:
+                if trigger_time.split(":")[0] == datetime.datetime.now().strftime("%H"):
+                    if trigger_time.split(":")[1] == datetime.datetime.now().strftime("%M"):
+                        self.play_alarm_sound()
+                        return
+                time.sleep(update_rate)
+        threading.Thread(target=funk_alarm, args=(trigger_time,), daemon=True).start()
+
+
+
+
+"""
+x = clock
+print(x.timer(5))
+
+x=clock().alarm(datetime.datetime.now().strftime("%H:%M"))
+
+clock().alarm("18:20")
+clock().timer(5)
+print("MEEE")
+time.sleep(4000000)
+"""
+
